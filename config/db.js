adfb88d747aeb6a5e60fd705d099b01acaa32180
mariapesm <youandmecanyousee@hotmail.com>
2018-12-31 14:56:06 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,6 +5,6 @@ const keys = require('./keys');
 mongoose.Promise = global.Promise;
 // Mongoose Connect
 mongoose
-  .connect('mongodb://<marype>:<networkengineering12>@ds145584.mlab.com:45584/pusherpullfcc')
+  .connect('mongodb://marype:networkengineering12@ds145584.mlab.com:45584/pusherpullfcc')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
