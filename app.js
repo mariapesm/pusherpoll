d3fd13bb248d2127c9ca1bdfbccbb1fa4d3298fd
mariapesm <youandmecanyousee@hotmail.com>
2018-12-31 16:14:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// DB Config
+//DB Config 
 require('./config/db');
 
 const app = express();
 
 const poll = require('./routes/poll');
 
-// Set public folder
+//set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Body parser middleware
+//body parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:false}));
 
-// Enable CORS
+//enable cors
 app.use(cors());
 
-app.use('/poll', poll);
+app.use('/poll', poll)
 
 const port = 3000;
 
-// Start server
-app.listen(port, () => console.log(`Server started on port ${port}`));
+//start server
+app.listen(process.env.PORT)
+    
+    //port, () => console.log(`Server started on port ${port}`));
